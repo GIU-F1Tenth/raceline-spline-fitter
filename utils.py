c29005d38f8129d6103185c1c95d2b48a4a17ebe
FamALouiz <fam@awadlouis.com>
2025-07-22 18:06:17 +0200

    feat: added core raceline spline fitter scripts

---
+++ utils.py
@@ -0,0 +1,43 @@
+import csv
+import os
+
+
+def catmull_rom_segment(p0, p1, p2, p3, t):
+    """
+    Calculate a point on a Catmull-Rom spline segment.
+    p0, p1, p2, p3 are control points, t is parameter [0,1]
+
+    Parameters:
+        p0: Control point before the segment start as an array or list of [x, y].
+        p1: Control point at the segment start as an array or list of [x, y].
+        p2: Control point at the segment end as an array or list of [x, y].
+        p3: Control point after the segment end as an array or list of [x, y].
+        t: Parameter value in the range [0, 1].
+
+    Returns:
+        A point on the spline as an array [x, y].
+    """
+    t2 = t * t
+    t3 = t2 * t
+
+    return 0.5 * ((2 * p1) +
+                  (-p0 + p2) * t +
+                  (2*p0 - 5*p1 + 4*p2 - p3) * t2 +
+                  (-p0 + 3*p1 - 3*p2 + p3) * t3)
+
+
+def save_path(output_csv, x_fine, y_fine):
+    """
+    Save the generated spline points to a CSV file.
+
+    Parameters:
+        output_csv: Path to the output CSV file.
+        x_fine: Array of x coordinates of the spline.
+        y_fine: Array of y coordinates of the spline.
+    """
+    os.makedirs(os.path.dirname(output_csv), exist_ok=True)
+    with open(output_csv, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['x', 'y'])
+        for x, y in zip(x_fine, y_fine):
+            writer.writerow([x, y])
